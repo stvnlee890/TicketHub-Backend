700b69c60604228428ba09ec375a425d550e845a
stvnlee890 <stvnlee890@gmail.com>
2022-12-06 14:37:26 -0800

    Drafting user schema

---
+++ model/users.js
@@ -0,0 +1,40 @@
+const mongoose = "../db/connection.js";
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    tickets: {
+      type: [String],
+    },
+    ticketQnty: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: (_doc, ret) => {
+        delete ret.password;
+        return ret;
+      },
+    },
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema); 
