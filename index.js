b0a53b460060b86565823e32f25a2fc0f34d91af
stvnlee890 <stvnlee890@gmail.com>
2022-12-06 19:06:33 -0800

    Checked for errors as to why Postman wasn't hitting route

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const cors = require("cors");
 // MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors)
+app.use(cors())
 
 // REDIRECT 
 app.get('/', (req, res) => {
-    res.redirect('/api/bookmarks');
+    res.redirect('/api/tickethub');
 })
 
 // CONTROLLERS
 const userController = require("./controller/UserController");
-app.use("/users", userController);
+app.use("/api/tickethub/users", userController);
 
 // PORT
 
-const PORT = process.env.PORT || 8800
-app.listen(PORT, () => console.log("=== BACKEND SERVER RUNNING. LOCAL HOST: 8800 ==="))
+const PORT = 8800
+app.listen(PORT, () => console.log(`=== BACKEND SERVER RUNNING. LOCAL HOST: ${PORT} ===`))
