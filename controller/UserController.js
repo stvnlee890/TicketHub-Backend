d41561e748b90683bf1c456e859cd0581cf7333c
stvnlee890 <stvnlee890@gmail.com>
2022-12-06 19:01:12 -0800

    Add bcrypt to hash user password

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,13 +1,19 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const User = require("../model/Users");
+const bcrypt = require("bcrypt");
+const { createUserToken } = require("../auth/auth");
 
-router.post("./signup", async (req, res, next) => {
-    try {
-        const user = await User.create({ ...req.body, password });
-        console.log(req.body)
-    } catch (error) {
-        console.log(error);
-    }
-})
+router.post("/signup", async (req, res, next) => {
+  try {
+    const password = await bcrypt.hash(req.body.password, 10);
+    const user = await User.create({ ...req.body, password });
+    console.log(req.body);
+    console.log(user);
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
