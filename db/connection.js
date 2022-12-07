e7bbce44b8f65339a1683293fd7789501b1c9676
stvnlee890 <stvnlee890@gmail.com>
2022-12-06 19:05:47 -0800

    Required dotenv

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,14 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-
 const mongoURI = process.env.DATABASE_URL;
 const db = mongoose.connection;
 
+mongoose.set('strictQuery', true);
 mongoose.connect(mongoURI);
 
 db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
 db.on("connected", () => console.log("mongo connected at: ", mongoURI));
 db.on("disconnected", () => console.log("mongo disconnected"));
-
 db.on("open", () => {
   console.log("✅ mongo connection made!");
 });
