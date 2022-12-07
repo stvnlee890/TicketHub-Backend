fd40da442f5a4453bf402d206b0a46f63c110914
stvnlee890 <stvnlee890@gmail.com>
2022-12-06 19:08:17 -0800

    Created auth for JWT token for user authentication and authorization

---
+++ auth/auth.js
@@ -0,0 +1,46 @@
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../model/Users");
+require("dotenv").config();
+
+const secret = process.env.JWT_SECRET;
+
+const { Strategy, ExtractJwt } = require("passport-jwt");
+
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: secret,
+};
+
+const strategy = new Strategy(options, async (jwt_payload, done) => {
+  try {
+    const user = await User.findById(jwt_payload.id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+passport.use(strategy);
+passport.initialize();
+
+const requireToken = passport.authenticate("jwt", { session: false });
+
+const createUserToken = (req, user) => {
+  if (
+    !user ||
+    !req.body.password ||
+    !bcrypt.compareSync(req.body.password, user.password)
+  ) {
+    const err = new Error("The provided username or password is incorrect");
+    err.statusCode = 422;
+    throw err;
+  }
+  return jwt.sign({ id: user._id }, secret, { expiresIn: 36000 });
+};
+
+module.exports = {
+    requireToken,
+    createUserToken,
+  };
