bb5b084925b74975539ec440a6b0e7e69cb494a9
stvnlee890 <stvnlee890@gmail.com>
2022-12-07 22:38:27 -0800

    Figuring out how to prevent same users from purchasing more than the ticket amount

--- controller/TicketController.js
+++ controller/TicketController.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const UserTicket = require("../model/UserTicket");
 const TicketDetails = require("../model/TicketDetails");
+const User = require("../model/Users")
 const router = express.Router();
 
 router.post("/ticket", async (req, res, next) => {
   const purchaseLimit = +req.body.userPurchaseLimit;
   const userPurchased = +req.body.userPurchased;
+  console.log(req.body.user)
 
   try {
     // find ticketDetails so we can decrement overall ticket count for that specific ticket type and event
     const findTicketDetails = await TicketDetails.findOne({
       concertId: req.body.concertId,
     });
+    const findTicket = await UserTicket.find({ user: req.body.user })
+    console.log(findTicket)
     // set up logic top level to immediately return if tickets aren't available
-    if (findTicketDetails.quantity <= 0) return res.sendStatus(404);
+    if (findTicketDetails.quantity <= 0 || userPurchased > findTicketDetails.quantity) return res.sendStatus(404);
     if (userPurchased > purchaseLimit) return res.sendStatus(406);
 
     // update ticket details so that client can receive proper ticket number
@@ -24,7 +28,7 @@ router.post("/ticket", async (req, res, next) => {
     // creating user ticket after user purchase
     const ticket = await UserTicket.create(req.body);
     await ticket.populate("user");
-    return res.status(200).json(ticketDetails);
+    return res.status(200).json(ticket);
   } catch (error) {
     console.log(error);
   }
