0a58393ae6c29bf1967c4e851c58355e2db000d9
stvnlee890 <stvnlee890@gmail.com>
2022-12-08 19:02:25 -0800

    switched find query to findOne so it doesn't return an array like structure, so bcrypt can confirm found user's password

--- controller/UserController.js
+++ controller/UserController.js
@@ -17,4 +17,17 @@ router.post("/signup", async (req, res, next) => {
   }
 });
 
+router.post("/login", async (req, res, next) => {
+  try {
+    // const password = await bcrypt.hash(req.body.password, 10);
+    const user = await User.findOne({ email: req.body.email });
+    // send token to secure routes for user profile for when they purchase their ticket
+    const token = createUserToken(req, user);
+    res.status(201).json({ token, user, status: 200 });
+  } catch (error) {
+    console.log(error);
+    return res.json(error)
+  }
+});
+
 module.exports = router;
