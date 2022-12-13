fab4e567937acf36901073c21ef9aebdce1dec83
stvnlee890 <stvnlee890@gmail.com>
2022-12-12 22:49:29 -0800

    changing the models and experimenting

--- model/UserTicket.js
+++ model/UserTicket.js
@@ -1,52 +1,39 @@
 const mongoose = require("../db/connection");
 
-const UserTicketSchema = new mongoose.Schema({
+const UserTicketSchema = new mongoose.Schema(
+  {
     concertTitle: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     ticketId: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     concertId: {
-        type: String,
-        required: true,
-    },
-    ticketType: {
-        type: String,
-        required: true,
-    },
-    venue: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
+      type: String,
+      required: true,
     },
     userPurchased: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     userPurchaseLimit: {
-        type: Number, 
-        required: true,
+      type: Number,
+      required: true,
     },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
     date: {
-        type: String,
-        required: true,
-    }
-
-},
-{
+      type: String,
+      required: true,
+    },
+  },
+  {
     timestamps: true,
-}
-
-)
+  }
+);
 
 module.exports = mongoose.model("UserTicketModel", UserTicketSchema);
